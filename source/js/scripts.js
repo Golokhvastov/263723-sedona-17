e3db4b62de347ec9b0d912c4315814136e889d3f
Golokhvastov Dmitry <dargon89@mail.ru>
2019-09-07 16:53:59 +0700

    подключил скрипт открытия меню на мобильной версии и ввел для этого классы

---
+++ source/js/scripts.js
@@ -0,0 +1,14 @@
+var navPage = document.querySelector('.page-nav');
+var navToggle = document.querySelector('.page-nav__toggle');
+
+navPage.classList.remove('page-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navPage.classList.contains('page-nav--closed')) {
+    navPage.classList.remove('page-nav--closed');
+    navPage.classList.add('page-nav--opened');
+  } else {
+    navPage.classList.add('page-nav--closed');
+    navPage.classList.remove('page-nav--opened');
+  }
+});
